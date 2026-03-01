5e48dc75a0ac659e39416d3d6a6f909c3d12b596
Krzysztof Mazurek <krzychu.mazurek@gmail.com>
2026-03-01 18:34:41 +0100

    ntfy - token

--- backend/src/main/kotlin/pl/kmazurek/infrastructure/notification/NtfyNotificationService.kt
+++ backend/src/main/kotlin/pl/kmazurek/infrastructure/notification/NtfyNotificationService.kt
@@ -16,6 +16,7 @@ import pl.kmazurek.application.service.NotificationService
 class NtfyNotificationService(
     @Value("\${ntfy.url}") private val ntfyUrl: String,
     @Value("\${ntfy.topic}") private val ntfyTopic: String,
+    @Value("\${ntfy.token}") private val ntfyToken: String,
 ) : NotificationService {
     private val logger = LoggerFactory.getLogger(NtfyNotificationService::class.java)
     private val restTemplate = RestTemplate()
@@ -32,6 +33,7 @@ class NtfyNotificationService(
             headers.contentType = MediaType.TEXT_PLAIN
             headers.set("Title", "Nowe konto w Poker Stats")
             headers.set("Tags", "wave,new")
+            headers.set("Authorization", "Bearer ${ntfyToken}")
 
             val body = "Nowy użytkownik: $userName ($userEmail)"
             val request = HttpEntity(body, headers)
